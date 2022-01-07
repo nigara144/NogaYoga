cd7336d1571813a62aff20fbce8cd518b531e825
nigara144 <nigar.azadaliyeva@gmail.com>
2022-01-08 00:07:39 +0200

    # User class created # Integration with firebase done # Sign up completed successfully # Login completed successfully # Email verification added # Profile and Home activities added # Logout implemented successfully # Profile screen design started # User details shown in profile

--- app/src/main/java/com/example/nogayoga/ProfileActivity.java
+++ app/src/main/java/com/example/nogayoga/ProfileActivity.java
@@ -1,17 +1,31 @@
 package com.example.nogayoga;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.data.SingleRefDataBufferIterator;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private Button logout;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +40,32 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(new Intent(ProfileActivity.this, MainActivity.class));
             }
         });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView userNameTextView = (TextView) findViewById(R.id.user_name);
+        final TextView emailTextView = (TextView) findViewById(R.id.email_profile);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                if(userProfile != null){
+                    String userName = userProfile.fullName;
+                    String email = userProfile.email;
+
+                    userNameTextView.setText(userName);
+                    emailTextView.setText(email);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, "Something wrong happened", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
